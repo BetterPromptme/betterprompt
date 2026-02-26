95398ee182eb1bb7b46b22ab453b28b543bd582a
Son <son.hua@cmon.io>
2026-02-26 13:44:34 +0700

    feat: integrate search command into CLI for enhanced skill querying

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,6 @@
 import { Command } from "commander";
 import { configCommand } from "./commands/config";
+import { searchCommand } from "./commands/search";
 import { CLI_MESSAGES, CLI_META } from "./constants";
 import { loadOrInitConfig } from "./core/config";
 
@@ -17,6 +18,7 @@ export const createProgram = (): Command => {
     });
 
   program.addCommand(configCommand);
+  program.addCommand(searchCommand);
 
   return program;
 };
